1db700850625910a1a63df6aef2fcbda89254efd
Maxence Poutord <maxence.poutord@gmail.com>
2019-06-03 11:01:33 +0700

    feat: convert to a Gatsby theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,4 +9,24 @@ module.exports = {
   },
   pathPrefix: config.pathPrefix,
   __experimentalThemes: ['gatsby-starter-morning-dew'],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: config.googleAnalyticsId,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: config.siteTitle,
+        short_name: config.siteTitle,
+        start_url: config.pathPrefix,
+        background_color: config.background_color,
+        theme_color: config.theme_color,
+        display: config.display,
+        icon: 'content/images/icon.jpeg',
+      },
+    },
+  ],
 }
