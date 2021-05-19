d01c897048ae467b3e4622239fc11766ca9f00ae
Maxence Poutord <maxence.poutord@gmail.com>
2021-05-19 21:58:22 +0200

    chore: yarn -> npm + remove dead code

--- src/components/FollowMe.js
+++ src/components/FollowMe.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import useSiteMetadata from '../hooks/use-site-config'
 import TwitterIcon from '../gatsby-theme-resume/components/icons/twitter'
