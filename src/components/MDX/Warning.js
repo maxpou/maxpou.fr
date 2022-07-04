ad970abdb1a3ccdae4a71584549d97956e16d194
Maxence Poutord <maxence.poutord@gmail.com>
2022-07-04 18:22:20 +0200

    post: publish turborepo

---
+++ src/components/MDX/Warning.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+import WarningIcon from '../icons/warning.js'
+
+const MessageWrapper = styled.aside`
+  color: var(--color-grey500);
+  box-sizing: border-box;
+  background-color: #fdf4ba;
+  border-left: 5px solid var(--color-secondary);
+  margin: 30px 0px;
+  padding: 20px;
+  border-radius: 0 8px 8px 0;
+`
+const MessageHeader = styled.div`
+  font-weight: bold;
+  & > svg {
+    position: relative;
+    top: 5px;
+    width: 25px;
+    height: 25px;
+    padding-right: 5px;
+  }
+`
+
+const Warning = ({ children, title }) => {
+  return (
+    <MessageWrapper>
+      <MessageHeader>
+        <WarningIcon />
+        {title}
+      </MessageHeader>
+      <div>{children}</div>
+    </MessageWrapper>
+  )
+}
+
+export default Warning
