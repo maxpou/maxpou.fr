52a8a31c3301348a38025967b16a319cec05800b
Maxence Poutord <maxence.poutord@gmail.com>
2024-11-14 15:02:20 +0100

    feat: update homepage + add YoutubeMate (#206)

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -1,25 +1,37 @@
 import DockerSymfonyImg from '../assets/projects/docker-symfony.png'
 import MorningDewImg from '../assets/projects/morning-dew.png'
 import MaxpouFrImg from '../assets/projects/website.jpg'
+import YoutubeMateImg from '../assets/projects/youtube-mate.jpg'
 
 export const projects = [
+  {
+    name: 'Youtube-Mate',
+    image: YoutubeMateImg,
+    description:
+      'An AI assistant that generates summaries of YouTube videos and saves you precious time!',
+    url: 'https://www.youtube-mate.com',
+    ghIcon: false,
+  },
   {
     name: 'gatsby-starter-morning-dew',
     image: MorningDewImg,
     description: 'A starter to build lightning-fast websites!',
     url: 'https://github.com/maxpou/gatsby-starter-morning-dew',
+    ghIcon: true,
   },
   {
     name: 'docker-symfony',
     image: DockerSymfonyImg,
     description:
-      'A Docker setup for developing applications with the Symfony framework. My most popular project!',
+      'A Docker setup for developing applications with the Symfony framework. My most popular GitHub project!',
     url: 'https://github.com/maxpou/docker-symfony',
+    ghIcon: true,
   },
   {
     name: 'maxpou.fr',
     image: MaxpouFrImg,
     description: 'This website! Built with Astro and Tailwind.',
     url: 'https://github.com/maxpou/maxpou.fr',
+    ghIcon: true,
   },
 ]
