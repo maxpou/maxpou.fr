cc433efc63e51c4c6f6f601f3b043c32fbc8520a
Maxence Poutord <maxence.poutord@gmail.com>
2026-09-13 16:12:23 +0200

    feat: update projects

---
+++ src/components/icons/Location.jsx
@@ -0,0 +1,20 @@
+const Location = props => (
+  <svg
+    role="img"
+    aria-hidden="true"
+    data-prefix="far"
+    data-icon="map-marker-alt"
+    className="svg-inline--fa fa-map-marker-alt fa-w-12"
+    viewBox="0 0 384 512"
+    width="1em"
+    height="1em"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M192 0C86.4 0 0 86.4 0 192c0 76.8 25.6 99.2 172.8 310.4 9.6 12.8 28.8 12.8 38.4 0C358.4 291.2 384 268.8 384 192 384 86.4 297.6 0 192 0zm0 448C51.2 246.4 48 243.2 48 192c0-79.4 64.6-144 144-144s144 64.6 144 144c0 51.2-3.2 54.4-144 256zm-80-256c0 44.2 35.8 80 80 80s80-35.8 80-80-35.8-80-80-80-80 35.8-80 80z"
+    />
+  </svg>
+)
+
+export default Location
---
+++ src/components/icons/Phone.jsx
@@ -0,0 +1,18 @@
+const Phone = props => (
+  <svg
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+    width="1em"
+    height="1em"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.13.96.36 1.9.69 2.79a2 2 0 0 1-.45 2.11L8.09 9.89a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.89.33 1.83.56 2.79.69A2 2 0 0 1 22 16.92z" />
+  </svg>
+)
+
+export default Phone
